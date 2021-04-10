1ba9fd4d253988527e36ee53bf732db2c2835456
Stéphane MEYER <tigerlost@me.com>
2021-04-10 21:51:05 +0200

    delete_sticker

--- src/statistics.sh
+++ src/statistics.sh
@@ -54,6 +54,17 @@ set_sticker() {
   return 1
 }
 
+delete_sticker() {
+  local uri param
+  uri="$1"
+  param="$2"
+  [[ $uri && $param ]] && {
+    cmd sticker delete song "$uri" "$param" || return 1
+    return 0
+  }
+  return 1
+}
+
 media_update() {
   __is_mpd_running || {
     echo > "$HOME/.config/currentmedia"
@@ -79,7 +90,7 @@ update_stats() {
   set_sticker "$uri" lastplayed "$(now)" || return 1
   
   local playcount
-  playcount="$(get_sticker "$uri" playcount)"
+  playcount="$(get_sticker "$uri" playcount 2> /dev/null)" || playcount=0
   ((playcount++))
   set_sticker "$uri" playcount "$playcount" || return 1
 
@@ -87,8 +98,6 @@ update_stats() {
 }
 
 reset_stats() {
-
-  __is_mpd_running || return 1
 
   local uri
   uri=$1
@@ -101,4 +110,51 @@ reset_stats() {
         return 0
 
   return 1
+}
+
+rating() {
+  # set current song rating.
+  # usage: rating [value]
+  # value must be an integer between 0 (unset) and 5.
+  # if no given value, print actual rating.
+
+  local cr uri
+  cr="$(get_sticker "$(getcurrent)" rating 2> /dev/null)" || cr=0
+  ((cr/=2))
+
+  [[ $1 ]] || {
+    case $cr in
+      0) echo "-----" ;;
+      1) echo "*----" ;;
+      2) echo "**---" ;;
+      3) echo "***--" ;;
+      4) echo "****-" ;;
+      5) echo "*****"
+    esac
+    return 0
+  }
+
+  uri="$(getcurrent)"
+
+  [[ $1 =~ ^[0-9]+$ ]] && {
+    local r="$1"
+    ((r<0 || r>5)) && {
+      __msg E "invalid value."
+      return 1
+    }
+    if [[ $r -eq 0 ]]; then
+      delete_sticker "$uri" rating || return 1
+      return 0
+    else
+      set_sticker "$uri" rating $((r*2))
+    fi
+  }
+}
+
+playcount() {
+  # print current song playcount.
+
+  local plc
+  plc="$(get_sticker "$(getcurrent)" playcount 2> /dev/null)" || plc=0
+  echo "$plc"
 }
