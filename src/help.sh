5b25a30c4abf4772e0502604ec2ad78d5d554442
Stéphane MEYER <tigerlost@me.com>
2021-04-14 23:18:17 +0200

    added help.

---
+++ src/help.sh
@@ -0,0 +1,74 @@
+# shellcheck shell=bash
+
+#
+# .▄▄ · • ▌ ▄ ·.  ▄▄▄· ▄▄·  ▄▄▄· super
+# ▐█ ▀. ·██ ▐███▪▐█ ▄█▐█ ▌▪▐█ ▄█ music
+# ▄▀▀▀█▄▐█ ▌▐▌▐█· ██▀·██ ▄▄ ██▀· player
+# ▐█▄▪▐███ ██▌▐█▌▐█▪·•▐███▌▐█▪·• client
+#  ▀▀▀▀ ▀▀  █▪▀▀▀.▀   ·▀▀▀ .▀    plus+
+#
+# This file is part of smpcp.
+# Copyright (C) 2021, Stéphane MEYER.
+#
+# Smpcp is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>
+#
+# HELP
+# C : 2021/04/13
+# M : 2021/04/14
+# D : Help.
+
+_help() {
+cat << EOB
+smpcp: version ${__version}
+
+Commands:
+  smpcp add <uri>                                  add song(s) to the queue.
+  smpcp addalbum [<artist> <album>]                append album to the queue.
+  smpcp clear                                      remove all songs from the queue.
+  smpcp cmd [-x] <command> [args]                  -
+  smpcp consume [off|on]                           set consume mode.
+  smpcp crop                                       remove all songs from the queue except current one.
+  smpcp delete <position> | <start-end>            delete song(s) from the queue.
+  smpcp dim                                        toggle volume dim.
+  smpcp getcurrent [format]                        show info about current song.
+  smpcp getduration [-h]                           display duration of current song.
+  smpcp getelapsed [-h]                            display elapsed time for current song.
+  smpcp getnext [format]                           show info about next song in the queue.
+  smpcp idle [event...]                            -
+  smpcp idleloop [event...]                        -
+  smpcp insertalbum [<artist> <album>]             add album after current song.
+  smpcp fcmd [-x] <command> [args] <filter>        -
+  smpcp ls                                         print queue.
+  smpcp move [<position> | <start-end>] <to>       move song(s) within the queue.
+  smpcp next                                       play next song in the queue.
+  smpcp pause                                      pause playback.
+  smpcp play [pos]                                 play song.
+  smpcp playalbum [<artist> <album>]               play album for current song.
+  smpcp prev                                       play song from the start or play previous song.
+  smpcp random [off|on]                            set repeat mode.
+  smpcp rating [0..5]                              rate song.
+  smpcp repeat [off|on]                            set repeat mode.
+  smpcp replaygain [mode]                          set replay gain mode
+  smpcp search <type> <query>                      search for songs.
+  smpcp searchadd <type> <query>                   search for songs and add them to queue.
+  smpcp seek [+|-]<[HH:[MM:]]SS> | [+|-]<0-100%>   seek to specified position.
+  smpcp single [off|on]                            set single mode.
+  smpcp status                                     print player status
+  smpcp stop                                       stop playback.
+  smpcp stop_after                                 stop playback after current song.
+  smpcp toggle [pos]                               toggle play/pause. play if paused.
+  smpcp vol [+|-]<vol>                             set volume.
+  smpcp xfade [duration]                           set crossfade duration.
+EOB
+}
