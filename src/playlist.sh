029d77fe26dd90e4e7d44e24eadbdd342759d77a
Stéphane MEYER <tigerlost@me.com>
2021-04-22 09:22:20 +0200

    queue is empty message now printed on stderr.

--- src/playlist.sh
+++ src/playlist.sh
@@ -25,7 +25,7 @@
 #
 # PLAYLIST
 # C │ 2021/04/03
-# M │ 2021/04/19
+# M │ 2021/04/22
 # D │ Queue management.
 
 list_queue() {
@@ -33,8 +33,8 @@ list_queue() {
   # usage: list_queue [-f [format]]
 
   queue_is_empty && {
-    __msg M "queue is empty."
-    return
+    >&2 __msg M "queue is empty."
+    return 1
   }
 
   local _cmd
