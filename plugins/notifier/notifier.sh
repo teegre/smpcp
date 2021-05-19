71d07906cbf986b8bca9252f10658bad52cfb3fd
Stéphane MEYER <tigerlost@me.com>
2021-05-20 00:08:03 +0200

    simple notification plugin.

---
+++ plugins/notifier/notifier.sh
@@ -0,0 +1,40 @@
+# shellcheck shell=bash
+
+#
+# .▄▄ · • ▌ ▄ ·.  ▄▄▄· ▄▄·  ▄▄▄· super
+# ▐█ ▀. ·██ ▐███▪▐█ ▄█▐█ ▌▪▐█ ▄█ music
+# ▄▀▀▀█▄▐█ ▌▐▌▐█· ██▀·██ ▄▄ ██▀· player
+# ▐█▄▪▐███ ██▌▐█▌▐█▪·•▐███▌▐█▪·• client
+#  ▀▀▀▀ ▀▀  █▪▀▀▀.▀   ·▀▀▀ .▀    plus+
+#
+# This file is a smpcp plugin.
+# Copyright (C) 2021, Stéphane MEYER.
+#
+# Smpcp is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>
+#
+# NOTIFIER
+# C : 2021/05/19
+# M : 2021/05/19
+# D : Display a notification on song change.
+
+export PLUG_NOTIFIER_VERSION="0.1"
+
+__plug_notifier_notify() {
+  case $1 in
+    add              ) notify-send -u low -i "$SMPCP_ICON" "[smpcp]" "Now adding new songs..." ;;
+    play|pause|change) notify-send -i "$(get_albumart)" "$(status)" ;;
+    stop             ) notify-send -i "$SMPCP_ICON" " stopped."
+  esac
+}
+
