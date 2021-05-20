61bd7f78ec1b8ab5d8fcb32db6cce50cf223370b
Stéphane MEYER <tigerlost@me.com>
2021-05-20 18:26:51 +0200

    /dev/urandom as a source for shuf.

--- src/lib/query.sh
+++ src/lib/query.sh
@@ -25,7 +25,7 @@
 #
 # QUERY
 # C │ 2021/04/05
-# M │ 2021/05/15
+# M │ 2021/05/20
 # D │ Music and sticker database query.
 
 # to achieve some advanced search we need to directly query
@@ -325,7 +325,7 @@ get_random_song() {
     QUEUE+=("$REPLY")
     ((count++))
     ((count==$1)) && break
-  done < <(shuf "$SMPCP_SONG_LIST" 2> /dev/null)
+  done < <(shuf --random-source /dev/urandom "$SMPCP_SONG_LIST" 2> /dev/null)
 }
 
 get_rnd() {
